0d25f58651efd0bc38d19268daa2f98890020390
Jan Werren <jan.werren@outlook.com>
2024-05-20 12:27:42 +0200

    feat: task 2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,26 @@
 package src;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Main {
+    public static Map<Integer, Integer> occurences = new HashMap<>();
+
     public static void main(String[] args) {
-        System.out.println(IOUtility.readFile("resources/in.txt"));
+        // fill map with 0s
+        for(int i = 0; i < 128; i++){
+            occurences.put(i, 0);
+        }
+
+        // read file and count occurences
+        var text = IOUtility.readFile("resources/in.txt");
+        for (var c : text.toCharArray()){
+            occurences.put((int)c, occurences.get((int)c) + 1);
+        }
+
+        // print occurences
+        for (var entry : occurences.entrySet()){
+            System.out.println((char)(int)entry.getKey() + " " + entry.getValue());
+        }
     }
 }
